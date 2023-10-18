e9f8ed14af98353583df2bd72645156641611528
OtavioFalcon <otavioprogramador017@gmail.com>
2023-10-18 17:38:05 -0300

    UP

--- Pasta_de_Teste/Teste.java
+++ Pasta_de_Teste/Teste.java
@@ -1,5 +1,21 @@
-package Pasta_de_Teste;
+import java.util.Scanner;
 
 public class Teste {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Insira algo (ou fim para sair):  ");
+            String var = scanner.nextLine();
+
+            if (var.equals("fim") || var.equals("ooo") || var.equals("FIM") || var.equals("Fim")) {
+                break;
+            }
+            
+            System.out.println("Sua resposta foi ( " + var + " ) ?");
+
+        }
+
+        scanner.close();
+    }
 }
